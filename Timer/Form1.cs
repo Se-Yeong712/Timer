4504ed5e4e1592809603aa9de8e23d182aacd332
이세영 <lsysy712@gmail.com>
2019-05-27 09:03:33 +0900

    #2 타이머 동작 코드 구현

--- Timer/Form1.cs
+++ Timer/Form1.cs
@@ -7,13 +7,80 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using Microsoft.VisualBasic;
+
 namespace Timer
 {
     public partial class Form1 : Form
     {
+        int CountOrgNum = 0;    
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnCount_Click(object sender, EventArgs e)
+        {
+            ProcessTimer();
+
+        }
+
+        private bool IntCheck()
+        {
+            if (Information.IsNumeric(this.txtNum.Text))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("숫자만 입력해 주세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (this.CountOrgNum < 1)
+            {
+                this.Timer.Enabled = false;
+                MessageBox.Show("펑", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtNum.ReadOnly = false;
+                this.txtNum.Text = "";
+                this.txtNum.Focus();
+            }
+            else
+            {
+                this.CountOrgNum = this.CountOrgNum - 1;
+                this.txtCountDown.Text = Convert.ToString(this.CountOrgNum);
+            }
+        }
+
+
+        private void TxtNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                ProcessTimer();
+            }
+        }
+
+
+        private void ProcessTimer()
+        {
+            if (this.CountOrgNum < 1)
+            {
+                this.Timer.Enabled = false;
+                MessageBox.Show("펑", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtNum.ReadOnly = false;
+                this.txtNum.Text = "";
+                this.txtNum.Focus();
+            }
+            else
+            {
+                this.CountOrgNum = this.CountOrgNum - 1;
+                this.txtCountDown.Text = Convert.ToString(this.CountOrgNum);
+            }
         }
     }
 }
